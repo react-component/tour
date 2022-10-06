ccd1eafbbcd29b55ba8453a09f5bb3d8edd8d98c
黑雨 <wangning4567@163.com>
2022-10-06 10:36:44 +0800

    feat: update ts

--- src/TourStep.tsx
+++ src/TourStep.tsx
@@ -55,7 +55,6 @@ const TourStep = (props: TourStepProps) => {
 
   const closeContent = (btnType: string | undefined) => {
     setCurrentStep(-1);
-    console.log('------', currentStep);
 
     if (typeof onClose === 'function') {
       onClose();
@@ -74,7 +73,9 @@ const TourStep = (props: TourStepProps) => {
   };
   const prevButtonNode: React.ReactNode = (
     <div className={`${prefixCls}-prevButton`} onClick={prevBtnClick}>
-      {prevButtonProps?.children}
+      {prevButtonProps?.children || (
+        <button className="ant-btn ant-btn-primary">上一步</button>
+      )}
     </div>
   );
 
@@ -87,7 +88,9 @@ const TourStep = (props: TourStepProps) => {
 
   const nextButtonNode: React.ReactNode = (
     <div className={`${prefixCls}-nextButton`} onClick={nextBtnClick}>
-      {nextButtonProps?.children}
+      {nextButtonProps?.children || (
+        <button className="ant-btn ant-btn-primary">下一步</button>
+      )}
     </div>
   );
 
@@ -100,7 +103,9 @@ const TourStep = (props: TourStepProps) => {
 
   const finishButtonNode: React.ReactNode = (
     <div className={`${prefixCls}-prevButton`} onClick={finishBtnClick}>
-      {finishButtonProps?.children}
+      {finishButtonProps?.children || (
+        <button className="ant-btn ant-btn-primary">结束引导</button>
+      )}
     </div>
   );
 
@@ -133,6 +138,23 @@ const TourStep = (props: TourStepProps) => {
       className={`${prefixCls}-close`}
     >
       <span className={`${prefixCls}-close-x`} />
+      <span
+        role="img"
+        aria-label="close"
+        className="anticon anticon-close ant-modal-close-icon"
+      >
+        <svg
+          viewBox="64 64 896 896"
+          focusable="false"
+          data-icon="close"
+          width="1em"
+          height="1em"
+          fill="currentColor"
+          aria-hidden="true"
+        >
+          <path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 00203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z" />
+        </svg>
+      </span>
     </button>
   );
 
