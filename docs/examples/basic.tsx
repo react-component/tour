a929d05d5a383308c8f9883fe37a76b907a64f82
黑雨 <wangning4567@163.com>
2022-10-10 15:31:22 +0800

    feat: update demo

--- docs/examples/basic.tsx
+++ docs/examples/basic.tsx
@@ -1,107 +1,98 @@
-import React, { Component, createRef } from 'react';
+import React, { useRef } from 'react';
 import Tour from '../../src/index';
 import './basic.less';
-import { placements } from '../../src/placements';
 
-interface RefObject<T> {
-  // immutable
-  readonly current: T | null;
-}
-class App extends Component {
-  private BtnRef: RefObject<HTMLButtonElement>;
-  private createBtnRef: RefObject<HTMLButtonElement>;
-  private updateBtnRef: RefObject<HTMLButtonElement>;
-  private deleteBtnRef: RefObject<HTMLButtonElement>;
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      destroyTourOnHide: false,
-      placement: 'right',
-      transitionName: 'rc-tour-zoom',
-      offsetX: placements.right.offset[0],
-      offsetY: placements.right.offset[1],
-      overlayInnerStyle: undefined,
-    };
-    this.BtnRef = createRef<HTMLButtonElement>();
-    this.createBtnRef = createRef<HTMLButtonElement>();
-    this.updateBtnRef = createRef<HTMLButtonElement>();
-    this.deleteBtnRef = createRef<HTMLButtonElement>();
-  }
-  componentDidMount() {}
-
-  render() {
-    return (
+const App = () => {
+  const createBtnRef = useRef<HTMLButtonElement>(null);
+  const updateBtnRef = useRef<HTMLButtonElement>(null);
+  const deleteBtnRef = useRef<HTMLButtonElement>(null);
+  return (
+    <div style={{ margin: 20 }}>
       <div>
-        <div>
-          <button className="ant-target" ref={this.createBtnRef}>
-            Create
-          </button>
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <button className="ant-target" ref={this.updateBtnRef}>
-            Update
-          </button>
-          <button className="ant-target" ref={this.deleteBtnRef}>
-            Delete
-          </button>
-        </div>
-
-        <Tour
-          defaultCurrent={2}
-          steps={[
-            {
-              title: '创建',
-              description: '创建一条数据',
-              target: () => this.createBtnRef.current,
-              mask: true,
-            },
-            {
-              title: '更新',
-              cover: (
-                <img
-                  src="https://avatars0.githubusercontent.com/u/9441414?s=200&v=4"
-                  alt="更新"
-                />
-              ),
-              description: (
-                <div>
-                  <span>更新一条数据</span>
-                  <button>帮助文档</button>
-                </div>
-              ),
-              mask: false,
-              target: () => this.updateBtnRef.current,
-            },
-            {
-              title: '删除',
-              cover: <video src="example.com" />,
-              description: (
-                <div>
-                  <span>危险操作：删除一条数据</span>
-                  <button>帮助文档</button>
-                </div>
-              ),
-              target: () => this.deleteBtnRef.current,
-              mask: true,
-              style: { color: 'red' },
-            },
-          ]}
-        />
+        <button
+          className="ant-target"
+          ref={createBtnRef}
+          style={{ marginLeft: 100 }}
+        >
+          Create
+        </button>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <button className="ant-target" ref={updateBtnRef}>
+          Update
+        </button>
+        <button className="ant-target" ref={deleteBtnRef}>
+          Delete
+        </button>
       </div>
-    );
-  }
-}
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Tour
+        defaultCurrent={2}
+        steps={[
+          {
+            title: '创建',
+            description: '创建一条数据',
+            target: () => createBtnRef.current,
+            mask: true,
+          },
+          {
+            title: '更新',
+            cover: (
+              <img
+                src="https://avatars0.githubusercontent.com/u/9441414?s=200&v=4"
+                alt="更新"
+              />
+            ),
+            description: (
+              <div>
+                <span>更新一条数据</span>
+                <button>帮助文档</button>
+              </div>
+            ),
+            mask: false,
+            target: () => updateBtnRef.current,
+          },
+          {
+            title: '删除',
+            cover: <video src="example.com" />,
+            description: (
+              <div>
+                <span>危险操作：删除一条数据</span>
+                <button>帮助文档</button>
+              </div>
+            ),
+            target: () => deleteBtnRef.current,
+            mask: true,
+            style: { color: 'red' },
+          },
+        ]}
+      />
+    </div>
+  );
+};
 
 export default App;
