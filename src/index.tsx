e826b7f4101db58f8616fe8121411d9be5bcc02c
二货机器人 <smith3816@gmail.com>
2022-10-12 14:58:58 +0800

    chore: clean up

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import Tour from './Tour';
-import TourStep from './TourStep';
 export type { TourProps } from './Tour';
-export type { TourStepProps } from './TourStep';
+export type { TourStepInfo } from './TourStep';
 
-export { TourStep };
 export default Tour;
