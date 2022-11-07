b056731d4b9c26edd1f95a706582aafff5c1d25a
黑雨 <wangning4567@163.com>
2022-11-07 23:49:05 +0800

    feat: update test

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -177,6 +177,65 @@ describe('Tour', () => {
     fireEvent.click(screen.getByRole('button', { name: 'Finish' }));
   });
 
+  it('animated', () => {
+    const Demo = () => {
+      const createBtnRef = useRef<HTMLButtonElement>(null);
+      const updateBtnRef = useRef<HTMLButtonElement>(null);
+      const deleteBtnRef = useRef<HTMLButtonElement>(null);
+      return (
+        <div style={{ margin: 20 }}>
+          <div>
+            <button ref={createBtnRef}>Create</button>
+            <div style={{ height: 200 }} />
+            <button ref={updateBtnRef}>Update</button>
+            <button ref={deleteBtnRef}>Delete</button>
+          </div>
+          <div style={{ height: 200 }} />
+
+          <Tour
+            defaultCurrent={1}
+            animated={{ placeholder: true }}
+            steps={[
+              {
+                title: '创建',
+                description: '创建一条数据',
+                target: () => createBtnRef.current,
+                mask: true,
+              },
+              {
+                title: '更新',
+                description: (
+                  <div>
+                    <span>更新一条数据</span>
+                    <button>帮助文档</button>
+                  </div>
+                ),
+                target: () => updateBtnRef.current,
+              },
+              {
+                title: '删除',
+                description: (
+                  <div>
+                    <span>危险操作:删除一条数据</span>
+                    <button>帮助文档</button>
+                  </div>
+                ),
+                target: () => deleteBtnRef.current,
+                mask: true,
+                style: { color: 'red' },
+              },
+            ]}
+          />
+        </div>
+      );
+    };
+    render(<Demo />);
+    fireEvent.click(screen.getByRole('button', { name: 'Prev' }));
+    fireEvent.click(screen.getByRole('button', { name: 'Next' }));
+    fireEvent.click(screen.getByRole('button', { name: 'Next' }));
+    fireEvent.click(screen.getByRole('button', { name: 'Finish' }));
+  });
+
   it('rootClassName', () => {
     const Demo = () => {
       const btnRef = useRef<HTMLButtonElement>(null);
