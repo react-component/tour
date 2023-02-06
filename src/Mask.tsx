76efa60c4facf66d97eb4604432f8fae463df96f
zhouzhenbiao <zhouzhenbiao@guanmai.cn>
2023-02-06 15:37:36 +0800

    fix: mask 样式，随窗口变化而变化

--- src/Mask.tsx
+++ src/Mask.tsx
@@ -62,7 +62,7 @@ const Mask = (props: MaskProps) => {
           >
             <defs>
               <mask id={maskId}>
-                <rect x="0" y="0" width="100%" height="100%" fill="white" />
+                <rect x="0" y="0" width="100vw" height="100vh" fill="white" />
                 {pos && (
                   <rect
                     x={pos.left}
