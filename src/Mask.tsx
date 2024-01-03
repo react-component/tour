4af4068f69335bfb81530157bbdf1f93171a9a7d
MadCcc <1075746765@qq.com>
2024-01-03 19:44:08 +0800

    fix: zoom should not break mask style in safari

--- src/Mask.tsx
+++ src/Mask.tsx
@@ -66,7 +66,7 @@ const Mask = (props: MaskProps) => {
           >
             <defs>
               <mask id={maskId}>
-                <rect x="0" y="0" width="100vw" height="100vh" fill="white" />
+                <rect x="0" y="0" width="100%" height="100%" fill="white" />
                 {pos && (
                   <rect
                     x={pos.left}
