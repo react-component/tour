b21c560c1c20981bb0b1cc87f8c552a854089978
kiner-tang <1127031143@qq.com>
2024-03-06 17:10:10 +0800

    feat: optimize code

--- src/hooks/useClosable.tsx
+++ src/hooks/useClosable.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { useMemo } from 'react';
 import type { TourProps } from '../interface';
 import type { TourStepInfo } from '../TourStep';
 
@@ -66,7 +65,7 @@ export function useClosable(
   closable: TourProps['closable'],
   closeIcon: TourProps['closeIcon'],
 ) {
-  return useMemo(() => {
+  return React.useMemo(() => {
     const stepClosableConfig = getClosableConfig(
       prefixCls,
       stepClosable,
