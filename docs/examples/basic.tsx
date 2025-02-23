471f50601d143dc18786757e93ff41d73e572629
小豪 <1844749591@qq.com>
2025-02-23 20:46:21 +0800

    revert doc

--- docs/examples/basic.tsx
+++ docs/examples/basic.tsx
@@ -2,22 +2,33 @@ import React, { useRef } from 'react';
 import Tour from '../../src/index';
 import './basic.less';
 
-const Demo = () => {
+const App = () => {
   const createBtnRef = useRef<HTMLButtonElement>(null);
   const updateBtnRef = useRef<HTMLButtonElement>(null);
   const deleteBtnRef = useRef<HTMLButtonElement>(null);
   return (
     <div style={{ margin: 20 }}>
       <div>
-        <button ref={createBtnRef}>Create</button>
+        <button
+          className="ant-target"
+          ref={createBtnRef}
+          style={{ marginLeft: 100 }}
+        >
+          Create
+        </button>
         <div style={{ height: 200 }} />
-        <button ref={updateBtnRef}>Update</button>
-        <button ref={deleteBtnRef}>Delete</button>
+        <button className="ant-target" ref={updateBtnRef}>
+          Update
+        </button>
+        <button className="ant-target" ref={deleteBtnRef}>
+          Delete
+        </button>
       </div>
+
       <div style={{ height: 200 }} />
 
       <Tour
-        defaultCurrent={1}
+        defaultCurrent={2}
         steps={[
           {
             title: '创建',
@@ -36,10 +47,21 @@ const Demo = () => {
             target: () => updateBtnRef.current,
           },
           {
+            title: '更新（无阴影）',
+            description: (
+              <div>
+                <span>更新一条数据</span>
+                <button>帮助文档</button>
+              </div>
+            ),
+            mask: false,
+            target: () => updateBtnRef.current,
+          },
+          {
             title: '删除',
             description: (
               <div>
-                <span>危险操作:删除一条数据</span>
+                <span>危险操作：删除一条数据</span>
                 <button>帮助文档</button>
               </div>
             ),
@@ -53,4 +75,4 @@ const Demo = () => {
   );
 };
 
-export default Demo;
+export default App;
